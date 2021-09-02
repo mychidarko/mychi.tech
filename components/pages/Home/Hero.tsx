6c1457b3b5fcc5487a9307d823ff4b9c036db092
Mychi <mickdd22@gmail.com>
2021-09-02 00:41:09 +0000

    :dizzy: added hero animations

--- components/pages/Home/Hero.tsx
+++ components/pages/Home/Hero.tsx
@@ -5,14 +5,27 @@ import heroStyles from "styles/components/hero.styles"
 
 const Hero = () => {
 	return (
-		<div css={heroStyles()} className="flex flex:center-between px-md-down:10">
+		<div
+			css={heroStyles()}
+			data-aos="fade-in"
+			className="flex flex:center-between px-md-down:10"
+		>
 			<div className="flex flex:center-between h:100 hero-content" css={contentCard()}>
 				<div className="hero-text w:50">
-					<h4 className="mb:_2">HI, MY NAME'S <br /><span>MICHAEL DARKO</span></h4>
-					<p>
+					<h4
+						className="mb:_2"
+						data-aos="fade-up"
+					>
+						HI, MY NAME'S <br /><span>MICHAEL DARKO</span>
+					</h4>
+					<p data-aos="fade-up" data-aos-delay="200">
 						I'm a full-stack software engineer with specialized experience in web application development and systems development, obsessed with functionality driven design and always open to learning 🚀
 					</p>
-					<div className="flex flex:center-between w:40 mt:_4">
+					<div
+						data-aos="fade-up"
+						data-aos-delay="400"
+						className="flex flex:center-between w:40 mt:_4"
+					>
 						<a href="https://twitter.com/mychidarko" target="_blank">
 							<Twitter size={20} />
 						</a>
@@ -27,7 +40,13 @@ const Hero = () => {
 						</a>
 					</div>
 				</div>
-				<img src="/me.jpg" alt="" className="hero-img w:40" />
+				<img
+					src="/me.jpg"
+					alt=""
+					className="hero-img w:40"
+					data-aos="fade-in"
+					data-aos-delay="200"
+				/>
 				<Particles
 					className="hero-particles"
 					params={{
