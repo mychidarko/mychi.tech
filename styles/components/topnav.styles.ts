08163e1d8298b4aefca0de869717c83981f05b26
Mychi <mickdd22@gmail.com>
2021-09-02 00:07:32 +0000

    :lipstick: updated topnav styles

--- styles/components/topnav.styles.ts
+++ styles/components/topnav.styles.ts
@@ -1,16 +1,21 @@
 import theme from "styles/theme";
 import { StyleFunction } from "./../@types/styles";
 
-const topNavStyles: StyleFunction = ({ active, hidden }: { active: boolean, hidden: boolean }) => ({
-	zIndex: 100,
+const topNavStyles: StyleFunction = ({
+	active,
+	hidden,
+	link,
+}) => ({
+	zIndex: hidden ? -1 : 100,
 	paddingLeft: "15%",
 	paddingRight: "15%",
 	color: theme.colors.white,
 	// color: theme.colors.primary,
 	width: "calc(100vw - 30%)",
-	display: hidden ? "none" : "flex",
+	opacity: hidden ? 0 : 1,
 	boxShadow: active ? theme.shadows.md : "none",
 	background: active ? theme.colors.primary : theme.colors.transparent,
+	transition: "ease-in all .2s",
 
 	h3: {
 		fontWeight: theme.typography.fontWeights.bold,
@@ -21,6 +26,10 @@ const topNavStyles: StyleFunction = ({ active, hidden }: { active: boolean, hidd
 	".topnav__links": {
 		div: {
 			cursor: "pointer",
+
+			"&.-active": {
+				color: theme.colors.red[400],
+			},
 
 			"&:not(:first-child)": {
 				marginLeft: 50,
