23421c9b2a3c1fa6ced4e5484aa3a103d22074cf
Mychi <mickdd22@gmail.com>
2021-09-02 00:06:58 +0000

    :sparkles: added blog page styles

---
+++ styles/pages/blog.styles.ts
@@ -0,0 +1,24 @@
+import theme from "styles/theme";
+import { StyleFunction } from "./../@types/styles";
+
+export const blogStyles: StyleFunction = () => ({
+	".page__content": {
+		marginTop: 120,
+		paddingLeft: "15%",
+		paddingRight: "15%",
+		paddingBottom: 50,
+		color: theme.colors.white,
+	},
+
+	".blog__welcome": {
+		img: {
+			...theme.presets.rounded,
+			width: 150,
+			height: 150,
+		},
+	},
+
+	".blog__list": {
+		flexWrap: "wrap",
+	},
+});
