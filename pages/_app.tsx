f232441779146ecb171138482d4b07e9d53f27c2
Mychi <mickdd22@gmail.com>
2021-09-02 00:35:40 +0000

    :heavy_plus_sign: added aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import GlobalStyles from "styles/global";
 import NProgress from 'nprogress';
 import router from 'next/router';
+import AOS from 'aos';
 
+import 'aos/dist/aos.css';
 import "449.css/dist/449.css";
 
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		AOS.init();
+	});
+
 	return (
 		<>
 			<GlobalStyles />
